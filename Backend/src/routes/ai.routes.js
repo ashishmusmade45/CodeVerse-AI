ff9d912caea6605ea46937e79624f3586148693c
Ashish Musmade <ashishmusmade45@gmail.com>
2025-10-05 19:00:12 +0530

    require prompt in controller

--- Backend/src/routes/ai.routes.js
+++ Backend/src/routes/ai.routes.js
@@ -1,12 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-router.get("/get-response",(req,res)=>{
-    const prompt = req.query.prompt; 
-
-    if(!prompt){
-        return res.status(400).send("Prompt is required");
-    }
-})
+router.get("/get-response",)
 
 module.exports = router;
