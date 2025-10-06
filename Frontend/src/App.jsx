596f3c95d38f5f3217a78dd2bf36be106e0133da
Ashish Musmade <ashishmusmade45@gmail.com>
2025-10-06 19:05:42 +0530

    add navbar feat

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,31 +1,38 @@
-import {useState, useEffect } from 'react';
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
 import Prism from "prismjs";
 import "prismjs/themes/prism-tomorrow.css";
 
-
 function App() {
-  const [count, setCount] = useState(0)
-
-useEffect(()=>{
-  Prism.highlightAll()
-})
+  useEffect(() => {
+    Prism.highlightAll();
+  }, []);
 
   return (
-    <main>
-      <div className="left">
-        <div className="code">
-          <pre>
-            <code className="language-javascript">
-                
-            </code>
-          </pre>
+    <div className="app-wrapper">
+      <nav className="navbar">
+        <h1 className="logo">
+          <span className="white">CodeVerse</span>
+          <span className="violet">-AI</span>
+        </h1>
+      </nav>
+
+      <main className="main">
+        <div className="left">
+          <div className="code">
+            <pre>
+              <code className="language-javascript">{`function sum() {
+  return 1 + 1;
+}`}</code>
+            </pre>
+          </div>
+          <div className="review">Review</div>
         </div>
-        <div className="review">Review</div>
-      </div>
-      <div className="right"></div>
-    </main>
-  )
+
+        <div className="right"></div>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
