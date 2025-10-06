eaa1e61c646e0e070de0fc8ec76df5c8a01c3075
Ashish Musmade <ashishmusmade45@gmail.com>
2025-10-06 18:35:12 +0530

    add left block code feat

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,26 @@
-import { useState } from 'react'
-import './App.css'
+import {useState, useEffect } from 'react';
+import './App.css';
+import Prism from "prismjs";
+import "prismjs/themes/prism-tomorrow.css";
+
 
 function App() {
   const [count, setCount] = useState(0)
 
+useEffect(()=>{
+  Prism.highlightAll()
+})
+
   return (
     <main>
       <div className="left">
-        <div className="code"></div>
+        <div className="code">
+          <pre>
+            <code className="language-javascript">
+                
+            </code>
+          </pre>
+        </div>
         <div className="review">Review</div>
       </div>
       <div className="right"></div>
